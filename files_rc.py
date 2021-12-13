64076089288bf6afe5cafaadbe802407491097ee
Aneta Cygan <aneta.cygan@vycorporation.com>
2021-12-13 18:04:34 -0500

    Remove remaining pyside imports.

--- files_rc.py
+++ files_rc.py
@@ -3,7 +3,8 @@
 # Created by: The Resource Compiler for Qt version 5.14.2
 # WARNING! All changes made in this file will be lost!
 
-from PySide2 import QtCore
+# from PySide2 import QtCore
+from qgis.PyQt import QtCore
 
 qt_resource_data = b"\
 \x00\x00\x07t\
@@ -56358,10 +56359,17 @@ qt_resource_struct = b"\
 \x00\x00\x01q\xd70\x01\xda\
 "
 
+
 def qInitResources():
-    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qRegisterResourceData(
+        0x03, qt_resource_struct, qt_resource_name, qt_resource_data
+    )
+
 
 def qCleanupResources():
-    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qUnregisterResourceData(
+        0x03, qt_resource_struct, qt_resource_name, qt_resource_data
+    )
+
 
 qInitResources()
